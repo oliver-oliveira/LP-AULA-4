2d11d39f3c032b352b04b0dcc66102034d0058af
oliver-oliveira <oliver.oliveira@aluno.ifsp.edu.br>
2026-03-25 20:20:46 -0300

    Add files via upload

--- loja.c
+++ loja.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-void main() {
+ int main() {
     float valor;
-    printf("Digite o valor total da compra:");
+    printf("Digite o valor total da compra: ");
     scanf("%f",&valor);
     
-    if (valor>=50.00) {
+    if (valor > 50.00) {
         printf("Voce ganhou um brinde!");
     }
+    return 0;
 }
